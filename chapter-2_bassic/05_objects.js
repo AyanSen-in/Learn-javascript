ad807daf2bb07292ee809208352ad5e282a72973
Ayan <ayansen.inf@gmail.com>
2026-09-24 19:52:02 +0530

    05_objects added

---
+++ chapter-2_bassic/05_objects.js
@@ -0,0 +1,25 @@
+const course ={
+    courseName: "js in hindi",
+    price : "999",
+    Insstructer: "hitesh"
+
+}
+
+// course.Insstructer
+const {Insstructer : instracter } = course
+
+console.log(Insstructer);
+console.log(instracter);
+
+
+
+// const navbar = ({company}) => {}
+
+// navbar(company = "hitesh")
+//destructuring(react)
+
+
+
+// sometimes api is in Array form
+
+// API in json format 
