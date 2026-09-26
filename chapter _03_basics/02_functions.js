513166346f100203fdece656edc310c5c80a6e89
Ayan <ayansen.inf@gmail.com>
2026-09-26 16:14:01 +0530

    02 functions done

---
+++ chapter _03_basics/02_functions.js
@@ -0,0 +1,37 @@
+function calculatecartPrice (val1 ,val2,...num1){   //val1,val2 takes first two value and rest are in the num1
+    return num1
+
+}
+
+// console.log(calculatecartPrice(200,855,500 ,8547));
+// ...(rest oporator )
+
+
+const user ={ 
+    username : "ayan",
+    price : 300
+}
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+
+}
+// handleobject(user)
+handleobject({
+    username: "sam ",
+    price: 5343
+})
+
+
+const myNewArray = [200,400,600]
+
+function returnsecoundvalue(getArray){
+    return getArray[1]
+
+}
+
+// console.log(returnsecoundvalue(myNewArray));
+// console.log(returnsecoundvalue([343,363456]));
+
+
+
